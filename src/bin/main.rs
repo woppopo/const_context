64f5f8dd3a4a1371fca19443624a3169ebe75a35
woppopo <woppopo@protonmail.com>
2022-09-22 18:07:38 +0900

    Update main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,9 +7,19 @@ use const_env::{ConstEnv, ConstVars};
 
 struct Name<const NAME: &'static str>;
 
+const fn modify(value: u32) -> u32 {
+    value + 32
+}
+
 const fn assign_u32<const NAME: &'static str, const VALUE: u32, const VARS: ConstVars>(
     _: ConstEnv<VARS>,
 ) -> ConstEnv<{ VARS.assign::<Name<NAME>, _>(VALUE) }> {
+    ConstEnv
+}
+
+const fn modify_u32<const NAME: &'static str, const VARS: ConstVars>(
+    _: ConstEnv<VARS>,
+) -> ConstEnv<{ VARS.assign::<Name<NAME>, _>(modify(VARS.get::<Name<NAME>, _>())) }> {
     ConstEnv
 }
 
@@ -20,7 +30,9 @@ fn main() {
         let v1 = env.get::<Name<"value">, u32>();
         let env = assign_u32::<"value", 8, _>(env);
         let v2 = env.get::<Name<"value">, u32>();
-        v1 + v2
+        let env = modify_u32::<"value", _>(env);
+        let v3 = env.get::<Name<"value">, u32>();
+        v1 + v2 + v3
     };
 
     println!("{}", value);
